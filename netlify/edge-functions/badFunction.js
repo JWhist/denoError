596e3f0023bcb7b0a0567db326786105bcfb608b
Jordan Whistler <whistler.5@osu.edu>
2022-06-06 13:21:47 -0400

    bad func

--- netlify/edge-functions/badFunction.js
+++ netlify/edge-functions/badFunction.js
@@ -1,4 +1,4 @@
 export default (req, context) => {
-  const func = new Function(req.headers.host);
-  console.log(eval(func));
+  const func = new Function("a", "b", 'return a + " " + b');
+  console.log(eval('func("hello", "world")'));
 };
