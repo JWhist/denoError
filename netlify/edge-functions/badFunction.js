394145d816c8018d43108d852c728d737553d3b2
Jordan Whistler <whistler.5@osu.edu>
2022-06-26 14:35:42 -0400

    update

--- netlify/edge-functions/badFunction.js
+++ netlify/edge-functions/badFunction.js
@@ -1,6 +1,3 @@
 export default (req, context) => {
-  context.log("IP TEST");
-  context.log(context.ip);
-  context.log("HEADER TEST");
-  context.log(req.headers.get("x-nf-client-connection-ip"));
+  context.log(context);
 };
